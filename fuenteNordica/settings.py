bb485d096c466241bfe9bc25ad7e6fd057c88361
Juan Jerez <jerezvelasquez.j@gmail.com>
2023-07-22 00:05:31 -0400

    Modificaciones para hostear aplicación en vercel

--- fuenteNordica/settings.py
+++ fuenteNordica/settings.py
@@ -120,9 +120,9 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# Default primary key field type
+# Default primary key field type 
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
